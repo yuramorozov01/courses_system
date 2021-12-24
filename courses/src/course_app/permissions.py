287378a4a4a3cdd6ef3a4909647650f6ff61de96
yuramorozov01 <yura_morozov01@mail.ru>
2021-12-25 01:13:58 +0300

    refactor: create base permission to check entry by a specified pk (CS-9)

--- courses/src/course_app/permissions.py
+++ courses/src/course_app/permissions.py
@@ -1,29 +1,8 @@
 from rest_framework import permissions
+from base_app.permissions import CustomBasePermission
 
 
-class CoursePermission(permissions.BasePermission):
-    '''Course base permission with retrieving entry of course in specified field.'''
-
-    def get_course_pk(self, view):
-        course_pk = view.kwargs.get('course_pk')
-        if course_pk is None:
-            course_pk = view.kwargs.get('pk')
-        return course_pk
-
-    def has_permission_by_field(self, request, view, field):
-        course_pk = self.get_course_pk(view)
-        if course_pk is not None:
-            try:
-                data_field = getattr(request.user, field)
-                data = data_field.all().filter(pk=course_pk)
-                if data.exists():
-                    return True
-            except AttributeError:
-                return False
-        return False
-
-
-class IsCourseAuthor(CoursePermission):
+class IsCourseAuthor(CustomBasePermission):
     '''Object-level permission to only allow author of a course to manipulate with data.
     Assumes the user model instance has an `own_courses` attribute.
     '''
@@ -31,10 +10,10 @@ class IsCourseAuthor(CoursePermission):
     message = 'Only author is allowed to do this.'
 
     def has_permission(self, request, view):
-        return self.has_permission_by_field(request, view, 'own_courses')
+        return self.has_permission_by_field(request, view, 'own_courses', 'course_pk')
 
 
-class IsCourseTeacher(CoursePermission):
+class IsCourseTeacher(CustomBasePermission):
     '''Object-level permission to only allow teachers of a course to manipulate with data.
     Assumes the user model instance has an `teaching_courses` attribute.
     '''
@@ -42,10 +21,10 @@ class IsCourseTeacher(CoursePermission):
     message = 'Only teachers are allowed to do this.'
 
     def has_permission(self, request, view):
-        return self.has_permission_by_field(request, view, 'teaching_courses')
+        return self.has_permission_by_field(request, view, 'teaching_courses', 'course_pk')
 
 
-class IsCourseStudent(CoursePermission):
+class IsCourseStudent(CustomBasePermission):
     '''Object-level permission to only allow students of a course to manipulate with data.
     Assumes the user model instance has an `attending_courses` attribute.
     '''
@@ -53,10 +32,10 @@ class IsCourseStudent(CoursePermission):
     message = 'Only students are allowed to do this.'
 
     def has_permission(self, request, view):
-        return self.has_permission_by_field(request, view, 'attending_courses')
+        return self.has_permission_by_field(request, view, 'attending_courses', 'course_pk')
 
 
-class IsCourseTeacherOrStudent(CoursePermission):
+class IsCourseTeacherOrStudent(CustomBasePermission):
     '''Object-level permission to only allow teachers and students of a course to manipulate with data.
     Assumes the user model instance has an `teaching_courses` and `attending_courses` attributes.
     '''
@@ -64,5 +43,5 @@ class IsCourseTeacherOrStudent(CoursePermission):
     message = 'Only teachers and students are allowed to do this.'
 
     def has_permission(self, request, view):
-        return self.has_permission_by_field(request, view, 'teaching_courses') or \
-               self.has_permission_by_field(request, view, 'attending_courses')
+        return self.has_permission_by_field(request, view, 'teaching_courses', 'course_pk') or \
+               self.has_permission_by_field(request, view, 'attending_courses', 'course_pk')

250ca2f7bbb47d31da70bd0028e8136fee57233d
yuramorozov01 <56312454+yuramorozov01@users.noreply.github.com>
2022-03-17 17:36:57 +0300

    feat: add collectstatic step in launching

--- courses/courses_run.bash
+++ courses/courses_run.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+python3 ./src/manage.py collectstatic --noinput
 python3 ./src/manage.py makemigrations
 python3 ./src/manage.py migrate
 python3 ./src/manage.py runserver 0.0.0.0:8000

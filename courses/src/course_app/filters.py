e8db54cbb2ac1492fa0dea1cb3b11b46ffce18c9
yuramorozov01 <56312454+yuramorozov01@users.noreply.github.com>
2022-03-29 18:10:56 +0300

    feat: add filtering by range and ordering on fields starts_at ends_at (CS-10)

--- courses/src/course_app/filters.py
+++ courses/src/course_app/filters.py
@@ -7,15 +7,21 @@ class CourseFilter(filters.FilterSet):
     id__in = filters.BaseInFilter(field_name='id')
 
     title = filters.CharFilter(field_name='title', lookup_expr='icontains')
-    starts_at_after = filters.DateFilter(field_name='starts_at', lookup_expr='gte')
-    ends_at_before = filters.DateFilter(field_name='ends_at', lookup_expr='lte')
+
+    starts_at= filters.DateFilter(field_name='starts_at')
+    ends_at = filters.DateFilter(field_name='ends_at')
+
+    starts_at_range = filters.DateFromToRangeFilter(field_name='starts_at')
+    ends_at_range = filters.DateFromToRangeFilter(field_name='ends_at')
 
     ordering = filters.OrderingFilter(
         fields=(
             ('title', 'title'),
+            ('starts_at', 'starts_at'),
+            ('ends_at', 'ends_at'),
         )
     )
 
     class Meta:
         model = Course
-        fields = ['title', 'starts_at', 'ends_at']
+        fields = ['id', 'title', 'starts_at', 'ends_at']

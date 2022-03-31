8930aee1b67cd4a5c6efb1dc24490be6e13c249f
yuramorozov01 <56312454+yuramorozov01@users.noreply.github.com>
2022-03-31 15:12:34 +0300

    feat: replace datetime with arrow package

--- courses/src/course_app/validators.py
+++ courses/src/course_app/validators.py
@@ -1,11 +1,11 @@
-from datetime import date
+import arrow
 
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
 
 def validate_date(value):
-    if value < date.today():
+    if value < arrow.now().date():
         raise ValidationError(
             _('%(value)s is not correct! Specify date that\'s not in the past!'),
             params={'value': value},

0f97c02ac316e59226fe32e6d708b7ac1d8879d0
yuramorozov01 <yura_morozov01@mail.ru>
2022-04-13 15:50:34 +0300

    feat: prevent purchasing courses that had been already purchased (CS-14)

--- courses/src/libs/payments/payment_service.py
+++ courses/src/libs/payments/payment_service.py
@@ -3,6 +3,7 @@ from course_app.models import Course
 from django.contrib.auth import get_user_model
 from libs.payments.exceptions import CustomerNotCreatedException
 from libs.payments.stripe_payment_service import StripePaymentService
+from payments_app.choices import PaymentStatusChoices
 from payments_app.models import Card, Customer, Payment
 from rest_framework.validators import ValidationError
 
@@ -50,6 +51,21 @@ class PaymentService:
             course = Course.objects.get(id=course_id)
         except Course.DoesNotExist:
             raise ValidationError({'course_id': 'Unknown course ID'})
+
+        if Payment.objects.filter(course=course, user=self._user, status=PaymentStatusChoices.SUCCEEDED).exists():
+            raise ValidationError({'course_id': 'You have already purchased this course!'})
+
+        process_statuses = [
+            PaymentStatusChoices.REQUIRES_CONFIRMATION,
+            PaymentStatusChoices.REQUIRES_ACTION,
+            PaymentStatusChoices.PROCESSING,
+            PaymentStatusChoices.REQUIRES_CAPTURE,
+        ]
+        if Payment.objects.filter(course=course, user=self._user, status__in=process_statuses).exists():
+            raise ValidationError({
+                'course_id': 'You cannot purchase this course until previous operation is completed'
+            })
+
         try:
             card = Card.objects.get(pm_id=pm_id)
         except Card.DoesNotExist:
